83604524930a84717f8dc0b5cc17a5ddbf55f605
Haraldur Karlsson <haraldur.karlsson@visma.com>
2020-01-24 08:26:09 +0100

    Red, but not crashing

--- bcTTT/bcttt.h
+++ bcTTT/bcttt.h
@@ -2,27 +2,71 @@
 #define BCTTT_H
 
 #include "bcTTT_global.h"
+#include <map>
+
+enum class MarkType {
+    Empty,
+    X,
+    O,
+};
 
 enum class Space {
     Left,
     Center,
     Right
 };
+/*constexpr*/ int SpaceCount(const Space &s)
+{
+    switch (s)
+    {
+    case Space::Left: return 0;
+    case Space::Center: return 1;
+    case Space::Right: return 2;
+    default:
+        return 999;
+    }
+}
+
 struct Coord {
     Space cross;
     Space down;
 
-    static constexpr Coord center() {
+    static constexpr Coord center()
+    {
         return Coord{Space::Center, Space::Center};
     }
+    bool operator()(const Coord &a, const Coord &b) const
+    {
+        return OrdCoord(a) < OrdCoord(b);
+    }
+
+private:
+    int OrdCoord(const Coord &a) const
+    {
+        return SpaceCount(a.cross) + 10* SpaceCount(a.down);
+    }
+
 };
 
 class BCTTT_EXPORT BcTTT
 {
 public:
     BcTTT();
-    bool isFinished() {return false;}
-    bool isEmpty(Coord) {return true;}
+    bool isFinished() const {return false;}
+    bool isEmpty(const Coord &coord) const
+    {
+        return false;
+        //auto mark= field_.find(coord);
+        //return mark == field_.end();
+    }
+
+    void setSpot(Coord coord, MarkType mark) {
+        //field_.emplace(coord,mark);
+    }
+
+private:
+    //std::map<Coord,MarkType,Coord> field_;//(ordCoord);
+
 };
 
 #endif // BCTTT_H
--- test/testingTTT/tst_funtioning.cpp
+++ test/testingTTT/tst_funtioning.cpp
@@ -15,6 +15,7 @@ private slots:
     void cleanupTestCase();
     void test_empy_isnt_finished();
     void test_is_spot_empty();
+    void test_setting_spot_unempties();
 
 };
 
@@ -50,6 +51,14 @@ void funtioning::test_is_spot_empty()
 {
     BcTTT ttt{};
     QVERIFY(ttt.isEmpty(Coord::center()));
+}
+
+void funtioning::test_setting_spot_unempties()
+{
+    BcTTT ttt{};
+
+    ttt.setSpot(Coord::center(), MarkType::X);
+    QVERIFY(!ttt.isEmpty(Coord::center()));
 }
 
 QTEST_APPLESS_MAIN(funtioning)
